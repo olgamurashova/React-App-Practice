72cca11365195e3421434559c60d6001910dee7d
Olga Murashova <om.dreyfus@gmail.com>
2022-11-07 22:35:22 -0500

    added objects to content

--- src/Content.js
+++ src/Content.js
@@ -2,8 +2,25 @@ import React from 'react';
 import { useState } from 'react';
 
 const Content = () => {
-  const [name, setName] = useState('Olga');
-  const [count, setCount] = useState(0);
+  const [items, setItems] = useState([
+    {
+      id: 1,
+      checked: false,
+      item: "One half pound bag of Cocoa covered almonds"
+    },
+    {
+      id: 2,
+      checked: false,
+      item: "React is not going well"
+    },
+  
+    {
+      id: 3,
+      checked: false,
+      item: "Ice-cream"
+    }
+  ]);
+ 
   
 
   const handleNameChange = () => {
@@ -12,29 +29,11 @@ const Content = () => {
     setName(names[int]);
    }
 
-   const handleClick = () => {
-    setCount(count + 1)
-    setCount(count + 1)
-    console.log(count)
-   }
-
-   const handleClick2 = () => {
-    console.log(count)
-   }
-
-   const handleClick3 = (event) => {
-    console.log(event.target.innerText)
-   }
 
 
   return (
     <main>
-        <p onDoubleClick={handleClick}>
-          Hello {name}!
-        </p>
-        <button onClick={handleNameChange}>Change name</button>
-        <button onClick={handleClick}>Click it</button>
-        <button onClick={handleClick2}>Click it</button>
+        
     </main>
   )
 }
