1b011146ff67e69c9e5626e738c01c59923a180d
Olga Murashova <om.dreyfus@gmail.com>
2022-11-07 23:01:42 -0500

    added styles

--- src/Content.js
+++ src/Content.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useState } from 'react';
+import { FaTrashAlt } from 'react-icons/fa';
 
 const Content = () => {
   const [items, setItems] = useState([
@@ -20,19 +21,27 @@ const Content = () => {
       item: "Ice-cream"
     }
   ]);
- 
-  
 
-  const handleNameChange = () => {
-    const names = ['Olga', 'Harry', 'Elena'];
-    const int = Math.floor(Math.random() * 3);
-    setName(names[int]);
-   }
 
 
 
   return (
     <main>
+      <ul>
+        {items.map((item)=> (
+
+            <li className='item' key={item.id}>
+              <input type="checkbox" checked={item.checked} />
+                <label>{item.item}</label>
+                <FaTrashAlt 
+                role="button" 
+                tabIndex="0" />
+            </li>
+            
+        )
+
+        )}
+      </ul>
         
     </main>
   )
