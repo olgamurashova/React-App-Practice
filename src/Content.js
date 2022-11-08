07b78e28d52545c4b790601f3efa1ff23dcea3f5
Olga Murashova <om.dreyfus@gmail.com>
2022-11-07 23:40:50 -0500

    added functions

--- src/Content.js
+++ src/Content.js
@@ -6,7 +6,7 @@ const Content = () => {
   const [items, setItems] = useState([
     {
       id: 1,
-      checked: false,
+      checked: true,
       item: "One half pound bag of Cocoa covered almonds"
     },
     {
@@ -23,25 +23,49 @@ const Content = () => {
   ]);
 
 
+const handleCheck = (id) => {
+  const listItems = items.map((item) => item.id ===id ? {...item, checked: !item.checked} : item );
+  setItems(listItems);
+  localStorage.setItem('shoppinglist', JSON.stringify(listItems));
+}
+
+const handleDelete = (id) => {
+  const listItems = items.filter((item) => item.id !== id);
+  setItems(listItems);
+  localStorage.setItem('shoppinglist', JSON.stringify(listItems));
+}
 
 
   return (
     <main>
+      {items.length ? (
       <ul>
         {items.map((item)=> (
 
             <li className='item' key={item.id}>
-              <input type="checkbox" checked={item.checked} />
-                <label>{item.item}</label>
+              <input 
+              type="checkbox" 
+              onChange={ ()=> handleCheck(item.id)}
+              checked={item.checked} />
+
+                <label
+                style={(item.checked) ? { textDecoration:'line-through'} : null}
+                onDoubleClick={() => handleCheck(item.id)}>
+                  
+                  {item.item}</label>
+
                 <FaTrashAlt 
+                onClick={() => handleDelete(item.id)}
                 role="button" 
                 tabIndex="0" />
             </li>
+        
             
-        )
-
-        )}
+        ))}
       </ul>
+      ) : (
+        <p style={{ marginTop: '2rem'}}>Your list is empty</p>
+      )}
         
     </main>
   )
